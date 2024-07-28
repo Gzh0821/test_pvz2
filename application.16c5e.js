b3fecee8a841f81a2e311240bdfb79937c12796d
Gzh0821 <gaozih0821@outlook.com>
2024-07-28 22:06:26 +0800

    wwp3-patch

--- application.16c5e.js
+++ application.16c5e.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.b8ad2.json';
+          this.settingsPath = 'src/settings.e9b86.json';
           this.showFPS = false;
         }
 
--- index.4a0a3.js
+++ index.4a0a3.js
@@ -1,4 +1,4 @@
-System.register(["./application.06b50.js"], function (_export, _context) {
+System.register(["./application.16c5e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.06b50.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application06b50Js) {
-      Application = _application06b50Js.Application;
+    setters: [function (_application16c5eJs) {
+      Application = _application16c5eJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
