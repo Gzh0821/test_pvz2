06f8e83c35240c15f36ba7311282eed86fc25047
Gzh0821 <gaozih0821@outlook.com>
2025-03-09 19:11:32 +0800

    update version 0.2.6

--- docs/tmpPatch.js
+++ docs/tmpPatch.js
@@ -1,39 +1,55 @@
 const electron = {
-    isFullscreen: () => {
-      return document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
-    },
-    enterFullscreen: (element = document.documentElement) => {
-      if (document.requestFullscreen) {
-        element.requestFullscreen();
-      } else if (element.mozRequestFullScreen) { // Firefox
-        element.mozRequestFullScreen();
-      } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
-        element.webkitRequestFullscreen();
-      } else if (element.msRequestFullscreen) { // IE/Edge
-        element.msRequestFullscreen();
+  isFullscreen: () => {
+    return document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
+  },
+  enterFullscreen: (element = document.documentElement) => {
+    if (document.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { // Firefox
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { // IE/Edge
+      element.msRequestFullscreen();
+    }
+  },
+  exitFullscreen: () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+      document.msExitFullscreen();
+    }
+  },
+  ipcRenderer: {
+    send: function (channel, ...data) {
+      switch (channel) {
+        case "e_isFullScreen":
+          return electron.isFullscreen();
+        case "e_fullScreen":
+          return electron.isFullscreen() ? electron.exitFullscreen() : electron.enterFullscreen();
+        case "e_window":
+          return electron.exitFullscreen();
+        case "e_openURL":
+          return electron.shell.openExternal(data[0]);
       }
     },
-    exitFullscreen: () => {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) { // Firefox
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) { // IE/Edge
-        document.msExitFullscreen();
+    sendSync: function (channel, data) {
+      switch (channel) {
+        case "e_isFullScreen":
+          return electron.isFullscreen();
       }
     },
-    ipcRenderer: {
-      send: function (channel, data) {
-        if (channel == "e_fullScreen") {
-          electron.isFullscreen() ? electron.exitFullscreen() : electron.enterFullscreen()
-        }
-      },
-      sendSync: function (channel, data) {
-      },
-      on: function (channel, callback) {
-      }
+    on: function (channel, callback) {
+    }
+  },
+  shell: {
+    openExternal: function (url) {
+      return window.open(url, '_blank');
     }
   }
-  window.electron = electron
+}
+window.electron = electron
